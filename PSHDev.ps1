6e26435835bcf666ea4a51ec86050ab3979da590
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-02 11:45:09 -0500

    Update PSHDev.ps1

--- PSHDev.ps1
+++ PSHDev.ps1
@@ -7,5 +7,5 @@ $env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
 choco install BoxStarter
 ."$env:APPDATA\BoxStarter\BoxStarterShell.ps1"
 
-"Installing QP Demo Configuration" | out-default
+"Installing PSH Dev Demo Configuration" | out-default
 Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/CSI-Windows/ToolConfigs/master/ConfigPshDev.Txt
