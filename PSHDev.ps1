ccd145a162acbf47926ef0c74fe9744fd44104fe
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-02 11:43:48 -0500

    Create PSHDev.ps1

---
+++ PSHDev.ps1
@@ -0,0 +1,11 @@
+"Getting Started..." | out-default
+set-executionpolicy bypass -force
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+$env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
+
+"Chocolately Installed, Now Installing BoxStarter" | out-default
+choco install BoxStarter
+."$env:APPDATA\BoxStarter\BoxStarterShell.ps1"
+
+"Installing QP Demo Configuration" | out-default
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/CSI-Windows/ToolConfigs/master/ConfigPshDev.Txt
