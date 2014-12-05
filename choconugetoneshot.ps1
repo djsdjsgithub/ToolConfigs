e89095016a85f137b15a0ddf77bfd9f8a6923d71
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-05 13:05:28 -0500

    Update choconugetoneshot.ps1

--- choconugetoneshot.ps1
+++ choconugetoneshot.ps1
@@ -1,5 +1,3 @@
-5 December 2014 11:24:31
-
 "***** > Getting Started..." | out-default
 set-executionpolicy bypass -force
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
