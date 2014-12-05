3a3496885f9c6405be41df9c0d5b4d0516a5ceb6
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-05 13:01:38 -0500

    Create choconugetoneshot.ps1

---
+++ choconugetoneshot.ps1
@@ -0,0 +1,103 @@
+5 December 2014 11:24:31
+
+"***** > Getting Started..." | out-default
+set-executionpolicy bypass -force
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
+$env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
+
+"***** > Chocolately Installed, Now Installing BoxStarter" | out-default
+choco install BoxStarter
+."$env:APPDATA\BoxStarter\BoxStarterShell.ps1"
+
+$IsDesktop = ((gwmi win32_operatingsystem).ProductType -eq 1)
+
+"***** > Chocolatey Server Install" | Out-Default
+cinst chocolatey.server
+
+"***** > Enable Web Services" | Out-Default
+
+If ($IsDesktop) {cinst webpi;$env:path += 'C:\Program Files\Microsoft\Web Platform Installer\'} else {import-module servermanager}
+
+If ($IsDesktop) {cinst IIS-WebServerRole -source WindowsFeatures} Else {add-windowsfeature Web-Server}
+If ($IsDesktop) {cinst ASPNET -source webpi} Else {add-windowsfeature Web-Asp-Net}
+If ($IsDesktop) {cinst DefaultDocument -source webpi} Else {add-windowsfeature Web-Default-Doc}
+If ($IsDesktop) {cinst DynamicContentCompression -source webpi} Else {add-windowsfeature Web-Dyn-Compression}
+If ($IsDesktop) {cinst HTTPRedirection -source webpi} Else {add-windowsfeature Web-Http-Redirect}
+If ($IsDesktop) {cinst IIS7_ExtensionLessURLs -source webpi} Else {}
+If ($IsDesktop) {cinst IISManagementConsole -source webpi} Else {add-windowsfeature Web-Mgmt-Console}
+If ($IsDesktop) {cinst ISAPIExtensions -source webpi} Else {add-windowsfeature Web-ISAPI-Ext}
+If ($IsDesktop) {cinst ISAPIFilters -source webpi} Else {add-windowsfeature Web-ISAPI-Filter}
+If ($IsDesktop) {cinst NETExtensibility -source webpi} Else {}
+If ($IsDesktop) {cinst RequestFiltering -source webpi} Else {add-windowsfeature Web-Filtering}
+If ($IsDesktop) {cinst StaticContent -source webpi} Else {add-windowsfeature Web-Static-Content}
+If ($IsDesktop) {cinst StaticContentCompression -source webpi} Else {add-windowsfeature Web-Stat-Compression}
+If ($IsDesktop) {cinst UrlRewrite2 -source webpi} Else {}
+
+#add-windowsfeature Web-Basic-Auth
+$IsOS62OrGreater = ([Environment]::OSVersion.Version -ge [Version]'6.2')
+
+If ($IsOS62OrGreater) {
+
+If ($IsDesktop) {cinst IIS-NetFxExtensibility45 -source WindowsFeatures} Else {Add-WindowsFeature NET-Framework-45-ASPNET}
+cinst NetFx4Extended-ASPNET45 -source WindowsFeatures
+If ($IsDesktop) {cinst IIS-ASPNet45 -source WindowsFeatures} Else {Add-WindowsFeature Web-Asp-Net45}
+
+} Else {
+cinst ASPNET_REGIIS -source webpi
+#."$env:windir\microsoft.net\framework\v4.0.30319\aspnet_regiis.exe" -i
+}
+
+"***** > Chocolatey Server Config" | Out-Default
+$packageName = 'chocolatey.server'
+$projectname = "ChocolatelyServer"
+$toolsDir = "C:\programdata\chocolatey\lib\chocolatey.server.0.1.1\tools"
+$webToolsDir = Join-Path $toolsDir $packageName
+$installDir = "C:\TOOLS"
+$webInstallDir = Join-Path $installDir $packageName
+
+
+If ($IsDesktop) {cinst ASPNET_REGIIS -source webpi} Else {}
+
+"***** > Web Server Config" | Out-Default
+Import-Module WebAdministration
+Remove-WebSite -Name "Default Web Site" -ErrorAction SilentlyContinue
+Remove-WebSite -Name "$projectname" -ErrorAction SilentlyContinue
+New-WebSite -ID 1 -Name "$projectname" -Port 80 -PhysicalPath "$webInstallDir" -Force
+ 
+$networkSvc = 'NT AUTHORITY\NETWORK SERVICE'
+"Setting folder permissions on `'$webInstallDir`' to 'Read' for user $networkSvc" | Out-default
+$acl = Get-Acl $webInstallDir
+$acl.SetAccessRuleProtection($False, $True)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("$networkSvc","Read", "ContainerInherit, ObjectInherit", "None", "Allow");
+$acl.AddAccessRule($rule);
+Set-Acl $webInstallDir $acl  
+   
+$webInstallAppDataDir = Join-Path $webInstallDir 'App_Data'
+"Setting folder permissions on `'$webInstallAppDataDir`' to 'Modify' for user $networkSvc" | Out-Default
+$acl = Get-Acl $webInstallAppDataDir
+$acl.SetAccessRuleProtection($False, $True)
+$rule = New-Object System.Security.AccessControl.FileSystemAccessRule("$networkSvc","Modify", "ContainerInherit, ObjectInherit", "None", "Allow");
+$acl.AddAccessRule($rule);
+Set-Acl $webInstallAppDataDir $acl
+
+# Import-Module WebAdministration
+$appPoolPath = "IIS:\AppPools\$projectName"
+#$pool = new-object
+"You can safely ignore the next error if it occurs related to getting an app pool that doesn't exist" | out-default
+$pool = Get-Item $appPoolPath
+if ($pool -eq $null) {
+  Write-Host "Creating the app pool `'$appPoolPath`'"
+   $pool = New-Item $appPoolPath 
+ }
+
+$pool.processModel.identityType = "NetworkService" 
+$pool | Set-Item
+Set-itemproperty $appPoolPath -Name "managedRuntimeVersion" -Value "v4.0"
+#Set-itemproperty $appPoolPath -Name "managedPipelineMode" -Value "Integrated"
+Start-WebAppPool "$projectName"
+"Creating the site `'$projectName`' with appPool `'$projectName`'" | out-default
+New-WebApplication "$projectName" -Site "$projectname" -PhysicalPath $webInstallDir -ApplicationPool "$projectName" -Force
+ 
+"Open proper firewall rules" | Out-Default
+netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80
+
