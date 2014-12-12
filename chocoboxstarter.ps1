e0440bedb6248fe479601f430cdbd949c8c5a3e7
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-12 16:15:13 -0500

    Update chocoboxstarter.ps1

--- chocoboxstarter.ps1
+++ chocoboxstarter.ps1
@@ -1,9 +1,16 @@
-"Getting Started..." | out-default
+
+Param (
+  [Parameter(Mandatory=$True,Position=1)][string]$ConfigURL
+  )
+
+"`r`n`r`nGetting Started..." | out-default
 set-executionpolicy bypass -force
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
 $env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
 
-"Chocolately Installed, Now Installing BoxStarter" | out-default
+"`r`n`r`nChocolately Installed, Now Installing BoxStarter" | out-default
 choco install BoxStarter
 ."$env:APPDATA\BoxStarter\BoxStarterShell.ps1"
 
+"`r`n`r`nKicking off box with given URL..." | out-default
+Install-BoxstarterPackage -PackageName $ConfigURL
