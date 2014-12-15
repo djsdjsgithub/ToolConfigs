fc2e6096d08c19bfb97b8c8d951cd0cb888c9d13
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-14 21:34:27 -0500

    Delete BASE-SRV.cmd

--- BASE-SRV.cmd
+++
@@ -1,1 +0,0 @@
-powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/CSI-Windows/ToolConfigs/master/BASE-SRV.ps1'))"
