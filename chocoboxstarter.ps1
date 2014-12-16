76de16795fe402acad98ff349207c5ca9a2a6c30
djsdjsgithub <djsdjsgithub.services@sanoys.com>
2014-12-15 19:15:29 -0500

    Update chocoboxstarter.ps1

--- chocoboxstarter.ps1
+++ chocoboxstarter.ps1
@@ -11,6 +11,3 @@ $env:path = "$($env:ALLUSERSPROFILE)\chocolatey\bin;$($env:Path)"
 "`r`n`r`nChocolately Installed, Now Installing BoxStarter" | out-default
 choco install BoxStarter
 ."$env:APPDATA\BoxStarter\BoxStarterShell.ps1"
-
-"`r`n`r`nKicking off box with given URL..." | out-default
-Install-BoxstarterPackage -PackageName  https://raw.githubusercontent.com/CSI-Windows/ToolConfigs/master/BASE-SRV.boxstart
